a1198fa05f03d23a52d584e9926a8e0c812f889e
Maisa <maisa.milena@gmail.com>
2022-09-15 22:42:25 -0300

    Initial project

---
+++ HelloFriend/HelloFriend/HelloFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloFriendApp.swift
+//  HelloFriend
+//
+//  Created by Maisa Milena on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
